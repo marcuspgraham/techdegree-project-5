415649f475b32fa13125c90fa2695d3b6ea8c62b
marcuspgraham <64594348+marcuspgraham@users.noreply.github.com>
2020-08-21 11:28:13 +0100

    Update 21/08/20_1

---
+++ js/testScript.js
@@ -0,0 +1,24 @@
+
+
+<script>
+document.getElementById("search").addEventListener("keyup", myFunction);
+
+function myFunction() {
+  var x = document.getElementById("search");
+  x.value = x.value.toLowerCase();
+  var x = document.getElementsByTagName("li")[0].getAttribute("class");
+
+    var str = "Hello world, welcome to the universe.";
+    var n = str.includes.toUpperCase("world");
+
+  for (var i = 0; i < 10; i += 1) {
+    // do something in here
+ } if {
+    .style.display = "";
+ } else {
+    .style.display = "none";
+ }
+}
+</script>
+
+document.getElementById("myUL")
