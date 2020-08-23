1da8720fe1570c1f4510610700047a9ebe76d2d2
marcuspgraham <64594348+marcuspgraham@users.noreply.github.com>
2020-08-23 15:33:26 +0100

    Update 23/08/20_1

--- js/testScript.js
+++ js/testScript.js
@@ -1,24 +1,26 @@
 
-
-<script>
-document.getElementById("search").addEventListener("keyup", myFunction);
-
+   //make sure to name the function based on what it does for readability 
 function myFunction() {
+  //again make sure to name variables based on the values they hold 
+  //here you're storing the search input in a variable  
   var x = document.getElementById("search");
+  //changing the value of the input to lowercase 
   x.value = x.value.toLowerCase();
+   //you've already created a variable named x so this should cause an error 
   var x = document.getElementsByTagName("li")[0].getAttribute("class");
-
     var str = "Hello world, welcome to the universe.";
     var n = str.includes.toUpperCase("world");
-
+// you don't want to have the loop end at 10 it should end when it has gone through each character 
   for (var i = 0; i < 10; i += 1) {
     // do something in here
+      //within the for loop you'll want to check if any of the terms match 
  } if {
     .style.display = "";
  } else {
     .style.display = "none";
  }
 }
-</script>
 
-document.getElementById("myUL")
+document.getElementById("search").addEventListener("keyup", myFunction);
+
+// document.getElementById("myUL")
