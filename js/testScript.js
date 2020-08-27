f107314231118a8d26e6ee43f85f7b24def655b3
marcuspgraham <64594348+marcuspgraham@users.noreply.github.com>
2020-08-27 09:11:05 +0100

    27/08/20_1

--- js/testScript.js
+++ js/testScript.js
@@ -5,13 +5,13 @@ function filter() {
     //Declare a variable that will get my images
     let input = document.getElementById('search');
     let filterValue = input.value.toUpperCase();
-    let images = document.getElementsByTagName('data-alt');
+    let images = document.getElementsByTagName('photo');
 
     //Loop through all my images
     //Look for a particular attribute on each image
     //If an image has that attribute, show it, if not, do not show it.
     for(i=0; i<images.length; i++) {
-        var a = images[i].getElementsByTagName('data-alt')[0];
+        var a = images[i].getElementsByTagName('photo')[0];
         if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
             images[i].style.display ="";
         }else {
@@ -19,3 +19,4 @@ function filter() {
         }
     }
 }
+
